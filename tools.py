54048f49e9a603c9def86fbe2dbeb5bbeb0b5c1a
adhu2018 <41824074+adhu2018@users.noreply.github.com>
2021-02-14 21:24:53 +0800

    add linkConverter

--- tools.py
+++ tools.py
@@ -250,6 +250,21 @@ def getClipboardData():
     win32clipboard.CloseClipboard()
     return data
 
+def linkConverter(link_):
+    linkList = {}
+    if link_.startswith("flashget://"):
+        linkList["real"] = flashgetLinkRestore(link_)
+    elif link_.startswith("qqdl://"):
+        linkList["real"] = qqdlLinkRestore(link_)
+    elif link_.startswith("thunder://"):
+        linkList["real"] = thunderLinkRestore(link_)
+    else:
+        linkList["real"] = link_
+    linkList["flashget"] = flashgetLinkGenerator(linkList["real"])
+    linkList["qqdl"] = qqdlLinkGenerator(linkList["real"])
+    linkList["thunder"] = thunderLinkGenerator(linkList["real"])
+    return linkList
+
 # md5(str[, encoding]) or md5(bytes) or md5(int)
 def md5(*_str):
     # 需要的模块：hashlib。
