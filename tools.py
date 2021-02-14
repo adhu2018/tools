e1dc1169936a05088aa74563d2e3ad953fedc892
adhu2018 <41824074+adhu2018@users.noreply.github.com>
2021-02-14 21:25:26 +0800

    add test

--- tools.py
+++ tools.py
@@ -477,4 +477,20 @@ def sendmail(username: str, password: str, smtp_host: str, smtp_port: int,
 
 
 if __name__ == "__main__":
+    print("部分功能测试（false表示存在异常）\n\r")
+    
+    temp = getClipboardData()
+    setClipboardData("test")
+    print("getClipboardData/setClipboardData: ", getClipboardData()=="test")
+    setClipboardData(temp)
+    
+    linkList = linkConverter("test")
+    print("flashgetLinkGenerator: ",
+        linkList["flashget"]=="flashget://W0ZMQVNIR0VUXXRlc3RbRkxBU0hHRVRd")
+    print("flashgetLinkRestore: ",
+        flashgetLinkRestore("flashget://W0ZMQVNIR0VUXXRlc3RbRkxBU0hHRVRd")=="test")
+    print("qqdlLinkGenerator: ", linkList["qqdl"]=="qqdl://dGVzdA==")
+    print("qqdlLinkRestore: ", qqdlLinkRestore("qqdl://dGVzdA==")=="test")
+    print("thunderLinkGenerator: ", linkList["thunder"]=="thunder://QUF0ZXN0Wlo=")
+    print("thunderLinkRestore: ", thunderLinkRestore("thunder://QUF0ZXN0Wlo=")=="test")
     pass
