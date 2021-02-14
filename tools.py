6eb5e272776ab3595eac80e22b30d8b6aff8c1aa
adhu2018 <41824074+adhu2018@users.noreply.github.com>
2021-02-14 21:23:39 +0800

    add flashgetLinkRestore

--- tools.py
+++ tools.py
@@ -220,6 +220,23 @@ def filter(url, path="blacklist.txt") -> str:
                 return ""
     return url
 
+def flashgetLinkRestore(link_: str):
+    # 需要的模块：base64, chardet。
+    # The required module: base64, chardet.
+    link = link_[11:]
+    if len(link) == 0 or not link_.startswith("flashget://"):
+        print("`{}`不是快车链接！".format(link_))
+        return None
+    bytes_ = base64.b64decode(link)
+    try:
+        str_ = bytes_.decode(chardet.detect(bytes_)['encoding'])
+    except TypeError:
+        try:
+            str_ = bytes_.decode("utf8")
+        except UnicodeDecodeError:
+            str_ = bytes_.decode("gbk")
+    return str_[10:-10]
+
 def getClipboardData():
     # 需要的模块：win32clipboard。
     # The required module: win32clipboard.
